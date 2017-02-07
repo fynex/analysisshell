3e8af157599458e8a2e13205f65a51d3744a57f0
fynex <fynex@users.noreply.github.com>
2017-02-07 17:27:19 +0100

    Update req.py

--- req.py
+++ req.py
@@ -1,6 +1,6 @@
 import socket
 
-def simple_send(ip, port, data, receive_size=2048):
+def single_send(ip, port, data, receive_size=2048):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.connect((ip,port))
     s.sendall(data)
     response = s.recv(receive_size)
