420782345e58f65baa366b4b73cd801a0f5b6f76
Context <context@base-7r5.contextis-testing.com>
2017-04-25 15:47:58 +0200

    added list handling

--- file.py
+++ file.py
@@ -10,3 +10,13 @@ def write(file_name, data):
         f.write(data)
 
 
+def read_list(file_name, delim="\n"):
+    fc = read(file_name)
+
+    return fc.split(delim)
+
+def write_list(str_collection, file_name, delim="\n"):
+    with open(file_name, "w") as f:
+        for entry in str_collection:
+            f.write(entry + delim)
+
