e3b0897fbad679ab262665f7d34efcc0cb210d00
Context <context@base-7r5.contextis-testing.com>
2017-04-25 15:59:56 +0200

    added json request

--- req.py
+++ req.py
@@ -1,4 +1,14 @@
 import socket
+import requests
+import json
+
+
+def json_post(url, json_dict):
+    #header = {"Content-Type": "application/json;charset=utf-8"}
+    header = {"Content-Type": "application/json"}
+
+    return requests.post(url, headers=header, data=json.dumps(json_dict)).text
+
 
 def single_send(ip, port, data, receive_size=2048, timeout_sec=5):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.connect((ip,port))
@@ -10,11 +20,11 @@ def single_send(ip, port, data, receive_size=2048, timeout_sec=5):
         return None
     finally:
         s.close()
-    
+
     return response
 
 def mass_send(ip_list, port_list, data, receive_size=2048, timeout_sec=5):
     for ip in ip_list:
         for port in port_list:
             print single_send(ip, int(port), data, receive_size)
-            
+
