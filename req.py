4b2490ce5a9b331a3a14249565667798f2b9eb87
fynex <fynex@users.noreply.github.com>
2017-11-14 11:11:04 +0100

    Update req.py

--- req.py
+++ req.py
@@ -28,3 +28,13 @@ def mass_send(ip_list, port_list, data, receive_size=2048, timeout_sec=5):
         for port in port_list:
             print single_send(ip, int(port), data, receive_size)
 
+
+def page_links(url, show=True):
+    r = requests.get(url)
+    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', r.text)
+    
+    if show:
+        for e in urls:
+            print(e)
+    
+    return urls
